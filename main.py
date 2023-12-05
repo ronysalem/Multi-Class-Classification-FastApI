50fbe66ad2fb8f813fd2b192c1c581018df3c800
ronysalem <ronysalem9@gmail.com>
2023-12-05 09:09:27 +0200

    modifying

--- main.py
+++ main.py
@@ -50,9 +50,9 @@ async def read_root(request: Request):
         
     return response
 
-if __name__ == "__main__":
-    port = int(getenv("PORT",8000))
-    uvicorn.run("main:app", host = '0.0.0.0', port = port, reload=True)
+# if __name__ == "__main__":
+#     port = int(getenv("PORT",8000))
+#     uvicorn.run("main:app", host = '0.0.0.0', port = port, reload=True)
         
     
     
